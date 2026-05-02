09e3363148cfc074eac166eabfd4efef02768681
Marcelo Akira Inuzuka <marcelo.akira@gmail.com>
2026-05-01 21:56:10 -0300

    chore(cli): bump ea11ctl version to 0.1.8

--- cli/ea11ctl.ps1
+++ cli/ea11ctl.ps1
@@ -5,7 +5,7 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
-$EA11CTL_FALLBACK_VERSION = '0.1.7'
+$EA11CTL_FALLBACK_VERSION = '0.1.8'
 $EA11CTL_OWNER = 'A11yDevs'
 $EA11CTL_REPO = 'emacs-a11y-vm'
 $EA11CTL_BRANCH = 'main'
